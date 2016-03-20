89bdbf211688c27696fda9d204e1f639e5d07794
Matt Paul <mattpaul001@icloud.com>
2016-03-20 20:03:52 +0000

    Set entries function added test passing

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,5 +1,5 @@
 import {expect} from 'chai';
-import {List} from 'immutable'
+import {List, Map} from 'immutable'
 
 describe('immutability', () => {
 
@@ -42,5 +42,36 @@ describe('immutability', () => {
 
   });
 
+  describe('a tree', () => {
+
+    function addMovie(currentState, movie) {
+      return currentState.update(
+        'movies', movies => movies.push(movie));
+    }
+
+    it('is immutable', () => {
+      let state = Map({
+        movies: List.of('Trainspotting', '28 Days Later')
+      });
+      let nextState = addMovie(state, 'Sunshine');
+
+      expect(nextState).to.equal(Map({
+        movies: List.of(
+          'Trainspotting',
+          '28 Days Later',
+          'Sunshine'
+        )
+      }));
+      expect(state).to.equal(Map({
+        movies: List.of(
+          'Trainspotting',
+          '28 Days Later'
+        )
+      }));
+    });
+
+  });
+
 });
+
 
