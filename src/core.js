2acc86a361162410ab9a15d787aa20b8345c4cbe
Matt Paul <mattpaul001@icloud.com>
2016-03-20 22:13:04 +0000

    Core logic done

--- src/core.js
+++ src/core.js
@@ -1,3 +1,48 @@
+import {List, Map} from 'immutable';
+
 export function setEntries(state, entries) {
-  return state.set('entries', entries);
+  return state.set('entries', List(entries));
+}
+
+export function next(state) {
+  const entries = state.get('entries');
+  return state.merge({
+    vote: Map({pair: entries.take(2)}),
+    entries: entries.skip(2)
+  });
+}
+
+export function vote(state, entry) {
+  //"reach into the nested data structure path ['vote', 'tally', 'Trainspotting'], and apply this function there. If there are keys missing along the path, create new Maps in their place. If the value at the end is missing, initialize it with 0". 
+  return state.updateIn(
+    ['vote', 'tally', entry],
+    0,
+    tally => tally + 1
+    );
+}
+
+
+function getWinners(vote) {
+  if (!vote) return [];
+  const [a,b] = vote.get('pair');
+  const aVotes = vote.getIn(['tally', a], 0);
+  const bVotes = vote.getIn(['tally', b], 0);
+  if (aVotes > bVotes) return [a];
+  else if (aVotes < bVotes) return [b];
+  else return [a,b];
+}
+
+export function next(state) {
+  const entries = state.get('entries')
+                       .concat(getWinners(state.get('vote')));
+  if(entries.size === 1) {
+    return state.remove('vote')
+                .remove('entries')
+                .set('winner', entries.first());
+  } else {
+    return state.merge({
+      vote: Map({pair: entries.take(2)}),
+      entries: entries.skip(2)
+    });
+  }
 }
