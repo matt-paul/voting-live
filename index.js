e1ec4a5923b770b17e0bb9c847844da7743aab07
Matt Paul <mattpaul001@icloud.com>
2016-03-21 14:22:11 +0000

    Server complete for now

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ import makeStore from './src/store';
 import startServer from './src/server';
 
 export const store = makeStore();
-startServer();
+startServer(store);
+
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries: require('./src/entries.json')
+});
+store.dispatch({type: 'NEXT'});
--- src/server.js
+++ src/server.js
@@ -2,4 +2,25 @@ import Server from 'socket.io';
 
 export default function startServer() {
   const io = new Server().attach(8090);
+
+  store.subscribe(
+    //JSON serialized snapshot of the state sent on state change
+    () => io.emit('state', store.getState().toJS())
+  );
+
+  //listen to connection events and emit state immediately to connecting device
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState().toJS());
+    socket.on('action', store.dispatch.bind(store));
+  });
 }
+
+//A client sends an action to the server.
+//The server hands the action to the Redux Store.
+//The Store calls the reducer and the reducer executes the logic related to the
+//action.
+//The Store updates its state based on the return value of the reducer.
+//The Store executes the listener function subscribed by the server.
+//The server emits a 'state' event.
+//All connected clients - including the one that initiated the original action -
+//receive the new state.
